a243fdef0b03fc1cb9600a6ba93302d012c7aa06
ZachariahKB <zachariahakb@gmail.com>
2024-04-22 17:48:48 -0700

    mod10

---
+++ tests/shape.test.js
@@ -0,0 +1,14 @@
+const { describe } = require("yargs")
+const {Square, Triangle, Circle} = require("../lib/shapes")
+const { default: test } = require("node:test")
+
+
+describe("Square", () => {
+    test( "will render svg for a green circle", () => {
+
+    })
+})
+
+test(should show a fillColor, () => {
+    const inputSVG
+}
